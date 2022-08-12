6dbb16af3826af7ba3b375e8dd0fef217ce02650
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-08-12 14:27:07 +0300

    completing-interface-injection

--- src/codewithcaleb/Main.java
+++ src/codewithcaleb/Main.java
@@ -6,14 +6,12 @@ public class Main {
         //it is the reponsibility of another class to inject these concrete implementations
         //it increases loose coupling //and only the necessary classes will have to be recompiled
         var calculator = new TaxCalculator2018(100_000);
-        var report = new TaxReport(calculator);
-        report.show();
+        var report = new TaxReport();
+        report.show(calculator);
 
 
         //changing dependency
-        report.setCalculator(new TaxCalculator2019());
-        report.show();
-
+        report.show(new TaxCalculator2019());
 
 
     }
--- src/codewithcaleb/TaxReport.java
+++ src/codewithcaleb/TaxReport.java
@@ -8,17 +8,17 @@ package codewithcaleb;
 public class TaxReport {
     //claculator is a dependency
     private TaxCalculator calculator;
-
-    //brining an interface via constructor injection
-    public TaxReport(TaxCalculator calculator) {
-       this.calculator = calculator;
-    }
-    public void  show(){
+//
+//    //brining an interface via constructor injection
+//    public TaxReport(TaxCalculator calculator) {
+//       this.calculator = calculator;
+//    }
+    public void  show(TaxCalculator calculator){
         var tax = calculator.calculateTax();
         System.out.println(tax);
     }
-    public void setCalculator(TaxCalculator calculator) {
-        this.calculator = calculator;
-    }
+//    public void setCalculator(TaxCalculator calculator) {
+//        this.calculator = calculator;
+//    }
 
 }
